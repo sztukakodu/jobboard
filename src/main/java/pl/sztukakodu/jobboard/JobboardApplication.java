ef74e613d29d643b3b2e6319c7a40c4b64cca8d1
Darek Mydlarz <dmydlarz@gmail.com>
2022-01-13 05:31:35 +0100

    Initialize repo

---
+++ src/main/java/pl/sztukakodu/jobboard/JobboardApplication.java
@@ -0,0 +1,13 @@
+package pl.sztukakodu.jobboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sztukakodu/jobboard/JobboardApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sztukakodu.jobboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
